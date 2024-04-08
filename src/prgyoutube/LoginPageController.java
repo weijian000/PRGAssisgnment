75d813132f920c7de7c749aa4cfe5b865f1e23ce
wei jian <weijian1311@gmail.com>
2024-04-09 01:04:44 +0800

    test

---
+++ src/prgyoutube/LoginPageController.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ */
+package prgyoutube;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+
+/**
+ *
+ * @author erza1
+ */
+public class LoginPageController implements Initializable {
+    
+    private Label label;
+    
+    private void handleButtonAction(ActionEvent event) {
+        /*Button button = (Button) actionEvent.getSource();
+        String s = button.getText();*/
+        
+        
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
